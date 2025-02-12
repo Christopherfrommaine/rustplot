15ff2c264fdbac615e4d4dbc4e4704942cd2c461
Christopherfrommaine <christopherfrommaine@gmail.com>
2025-02-11 19:41:52 -0500

    fixed module issue

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -4,7 +4,7 @@ mod test_array_plot;
 mod test_scatter_plot;
 mod test_func_plot_domain;
 mod test_axes;
-mod test_func_plot;
+mod test_function_plot;
 mod test_line_plot;
 mod test_image_plot;
 mod test_animation_plot;
